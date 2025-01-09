ff592e768ae7686bd8bc89529093c46aaa9e63b4
Alberto <185236986+Chispaclick@users.noreply.github.com>
2025-01-10 00:14:16 +0100

    Taste Share commit #15

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -6,12 +6,12 @@ import Link from "next/link";
 function NavBar({ toggleMenu, showLogin }) {
   return (
     <div className="flex items-center place-content-between bg-background p-4 border-b-[0.2px] border-b-gray-300">
-      <HeroiconsBars4Solid onClick={toggleMenu} />
+      <HeroiconsBars4Solid onClick={toggleMenu} className="ml-2"/>
       <Link href="/taste-share">
         <img src="/logo_taste_share.png" className="w-20 ml-6" />
       </Link>
       <Link href="/log">
-        <button className="bg-gray-300 text-gray-950 font-semibold mb-2 rounded-lg py-1 px-3 "
+        <button className="bg-gray-300 text-gray-950 font-semibold  rounded-lg py-1 px-3 mr-2"
         onClick={ showLogin }>
           Login
         </button>
