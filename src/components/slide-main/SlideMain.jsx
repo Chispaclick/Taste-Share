df16646ba3ed9253834152a75463f68cc6dcbae0
Alberto <185236986+Chispaclick@users.noreply.github.com>
2025-01-09 17:11:57 +0100

    Taste Share commit #9

--- src/components/slide-main/SlideMain.jsx
+++ src/components/slide-main/SlideMain.jsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 
 
-function SlideMain({ isOpen }) {
+function SlideMain({ isOpen, setIsOpen }) {
+  const handleClose = () => {
+    setIsOpen(false); // Cierra el menú al hacer clic en un enlace
+  };
   return (
     <div
       className={`absolute z-{0} left-0 top-18 flex flex-col bg-background text-white p-2 rounded shadow-lg transition-transform duration-500 ease-in-out w-full overflow-hidden${
@@ -16,42 +19,49 @@ function SlideMain({ isOpen }) {
       </di>
       <div className="flex flex-col p-4 pb-96">
         <Link
+          onClick={handleClose}
           className="text-gray-300 text-lg p-1 hover:bg-gray-100 hover:text-gray-950 pl-4 rounded-xl font-semibold mb-2"
           href="/"
         >
           Home
         </Link>
         <Link
+          onClick={handleClose}
           className="text-gray-300 text-lg p-1 hover:bg-gray-100 hover:text-gray-950 pl-4 rounded-xl font-semibold mb-2"
           href="/profile"
         >
           Profile
         </Link>
         <Link
+          onClick={handleClose}
           className="text-gray-300 text-lg p-1 hover:bg-gray-100 hover:text-gray-950 pl-4 rounded-xl font-semibold mb-2"
           href="/ranking"
         >
           Ranking
         </Link>
         <Link
+          onClick={handleClose}
           className="text-gray-300 text-lg p-1 hover:bg-gray-100 hover:text-gray-950 pl-4 rounded-xl font-semibold mb-2"
           href="/friends"
         >
           Friends
         </Link>
         <Link
+          onClick={handleClose}
           className="text-gray-300 text-lg p-1 hover:bg-gray-100 hover:text-gray-950 pl-4 rounded-xl font-semibold mb-2"
           href="/trend"
         >
           Trend
         </Link>
         <Link
+          onClick={handleClose}
           className="text-gray-300 text-lg p-1 hover:bg-gray-100 hover:text-gray-950 pl-4 rounded-xl font-semibold mb-2"
           href="/subscription"
         >
           Subscription
         </Link>
         <Link
+          onClick={handleClose}
           className="text-gray-300 text-lg p-1 hover:bg-gray-100 hover:text-gray-950 pl-4 rounded-xl font-semibold mb-2"
           href="/settings"
         >
