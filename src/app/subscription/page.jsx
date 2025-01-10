e52bab17fe68782b0e9872485e22c2398f378be5
Alberto <185236986+Chispaclick@users.noreply.github.com>
2025-01-10 01:47:58 +0100

    Taste Share commit #17

--- src/app/subscription/page.jsx
+++ src/app/subscription/page.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from "react";
 
 function Suscription() {
   return (
+    <div className="flex bg-background place-content-center items-center p-8 w-full">
     <div>
-      <h1>Subscription Page</h1>
+      {/* Introducción de la página */}
+      <div>
+        <h1 className="text-gray-300 text-3xl font-semibold mb-4">
+          Subscriptions
+        </h1>
+        <p className="text-gray-300 text-lg mb-6">
+          Elige el plan que mejor se adapte a tus necesidades y comienza a disfrutar de los beneficios exclusivos que tenemos para ti. Con cualquiera de nuestras suscripciones, obtendrás acceso a contenido exclusivo y herramientas avanzadas que mejorarán tu experiencia.
+        </p>
+      </div>
+      
+      <div>
+        {/* Plan económico */}
+        <div className="flex flex-col flex-1 place-content-center items-start p-8 bg-gray-300 rounded-md mb-6">
+          <p className="text-gray-950 font-semibold mb-2">Full Plan</p>
+          <h2 className="text-gray-950 text-4xl mb-4">$5.00</h2>
+          <p className="text-gray-950 font-semibold mb-4">
+            Ideal para quienes quieren disfrutar de una experiencia básica pero completa.
+          </p>
+          <button className="bg-background text-gray-300 p-2 rounded-md w-full">
+            Buy this plan
+          </button>
+          {/* Beneficios del plan económico */}
+          <p className="text-gray-950 font-semibold mt-4">Incluye:</p>
+          <ul className="text-gray-950">
+            <li>- Acceso a contenido exclusivo.</li>
+            <li>- Recomendaciones personalizadas.</li>
+            <li>- Soporte técnico prioritario.</li>
+            <li>- Almacenamiento en la nube para tu contenido.</li>
+          </ul>
+        </div>
+  
+        {/* Plan premium */}
+        <div className="flex flex-col flex-1 place-content-center items-start p-8 bg-amber-600 rounded-md">
+          <p className="text-gray-950 font-semibold mb-2">Premium Plan</p>
+          <h2 className="text-gray-950 text-4xl mb-4">$10.00</h2>
+          <p className="text-gray-950 font-semibold mb-4">
+            Perfecto para usuarios que desean la mejor experiencia con todos los beneficios premium.
+          </p>
+          <button className="bg-background text-gray-300 p-2 rounded-md w-full">
+            Buy this plan
+          </button>
+          {/* Beneficios del plan premium */}
+          <p className="text-gray-950 font-semibold mt-4">Incluye:</p>
+          <ul className="text-gray-950">
+            <li>- Todo lo del Full Plan.</li>
+            <li>- Acceso a herramientas avanzadas.</li>
+            <li>- Mayor almacenamiento en la nube.</li>
+            <li>- Contenido adicional exclusivo.</li>
+            <li>- Posibilidad de compartir tu cuenta con un amigo.</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  </div>
+  );
 }
 
-export default Suscription
+export default Suscription;
